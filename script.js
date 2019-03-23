a5f650b362c074e8f6663673a341943cb9bc8a56
Sharon <sharon@Sharon-MBP.local>
2019-03-23 17:00:28 +0800

    new

--- script.js
+++ script.js
@@ -1,111 +1,114 @@
-$(document).ready(function () {
+$(document).ready(function () {  
+	let list = [
+		{content:'item1',mark:0},
+		{content:'<h1>item2</h1>',mark:1}
+	] 
+	render()
 
-/* navbar control */
-	function countItem(){
-		let all = Number($('.row-item').length)
-		let checked = Number( $('div[mark="item-checked"]').length )
-		let left = Number(all-checked)
-		$('nav > span').html(`${left} items left`) // left items 
-
-		// 完全沒 item
-		if( $('#show').html() === 'All' ){
-			$('#noItem').html('Add some items')			
-			if( all === 0 ){
-				$('#noItem').show()
-			}else{
-				$('#noItem').hide()
-			}
-		}else if( $('#show').html() === 'Active' ){
-			$('#noItem').html('No active items')	
-			if( left === 0 ){
-				$('#noItem').show()
-			}else{
-				$('#noItem').hide()
-			}
-		}
-	}
-	countItem();
-
-	function showAll(){
-		$('div[mark="item-checked"]').parent().fadeIn(200)
-		$('.show-all').attr('id' , 'show')
-		$('.show-active').removeAttr('id')	
-	}
-	function showAtcive(){
-		$('div[mark="item-checked"]').parent().fadeOut(200)
-		$('.show-active').attr('id' , 'show')
-		$('.show-all').removeAttr('id')
-	}
-	$('nav').on('click', '.show-all', function(){ 
-		showAll();
-		countItem();
-	});
-	$('nav').on('click', '.show-active', function(){ 
-		showAtcive();
-		countItem();
-	});
-
-/* add item */
-	$('body').on('click', '#btn-add', function(){
-
+	function addTodo(todo) { 
 		let inputArea = $('.row-addItem input')
-		let inputText = document.createTextNode( inputArea.val() );
-
-		if( !inputArea.val()){
+		if( !inputArea.val() ){
 			inputArea.attr('placeholder','I need a name for the item...');
 		}else{
-			let prependAdd = `
-				<div class="row-item" id="new">
-					<div class="input-text"></div>
-					<div class="btn-delete">－</div>
-				</div>
-			`
-			$( '.listBlock' ).prepend( prependAdd );
-			$('#new .input-text').append(inputText);
+			list.push( {content:`${inputArea.val()}`,mark:0} )
+			console.log(list)
+	 		render( pageNow() )
+		}
+	}
+	
+	function removeTodo(id) {
+	  list.splice(id, 1);  
+	  render( pageNow() )
+	}
 
-			$('#new').hide().fadeIn( 600,countItem() ).removeAttr('id');
+	function pageNow(){
+		if( $('.show-active').attr('class') ==='show-active show' ){
+			return 'active'
+		}
+	}
 
-			inputArea.val('')
-			inputArea.attr('placeholder','What needs to be done?');
+	function render(page){ 
+console.log(page)
+		$('.row-addItem input').val('').attr('placeholder','What needs to be done?')
+	  $('.listBlock').empty()
+		$('nav > span').html(`${list.length} items left`)
+	
+		list.forEach( (item,i) => {
+			let itemContent = document.createTextNode( item.content )
+			let area = `
+					<div class="row-item" order="${i}">
+						<div class="input-text"></div>
+						<div class="btn-delete">－</div>
+					</div>`
+			$('.listBlock').append(area)
+			$(`[order='${i}'] .input-text`).append(itemContent);
+
+			if( item.mark ===1 && page === 'active'){
+				//marked:display 
+				$(`[order='${i}']`).css('display','none')
+			}else if( item.mark ===1 &&  page !== 'active'){
+				//marked:del
+				$(`[order='${i}'] .input-text`).attr('mark','item-checked')
+				$(`[order='${i}'] .input-text`).html(`<del></del>`)
+				$(`[order='${i}'] del`).append(itemContent);
+			}
+		})
+
+		// no active item
+		if(page === 'active' && list.every(item => item.mark === 1) ){  
+			$('.listBlock').append(`<div id="noItem"> No active item </div>`)
+		}
+		//no item
+		if( page !== 'active' && list.length === 0 ){ 
+			$('.listBlock').append(`<div id="noItem"> Add some items </div>`)
+		}
+	}
+
+	/* add item */
+	$('body').on('click', '#btn-add', addTodo);
+
+	/* delete item */
+	$('.listBlock').on('click', '.btn-delete', function(e){
+		let order = $(e.target).parent().attr('order')		
+		removeTodo(order)
+	});
+
+	/* change page */
+	$('nav').on('click', 'span', function(e){ 
+		if( $(e.target).attr('class') === 'show-active'){ 
+			$('.show-all').attr('class' , 'show-all')
+			$('.show-active').attr('class' , 'show-active show')
+			render('active')
+
+		}else if( $(e.target).attr('class') === 'show-all'){
+			$('.show-all').attr('class' , 'show-all show')
+			$('.show-active').attr('class' , 'show-active')
+			render()
 		}
 	});
 
-/* mark or not */
+	/* mark item */
 	$('.listBlock').on('click', '.input-text', function(e){
 		let target = $(e.target)
-		if( target.is('div') ){
-			if( target.children().is('del') ){
-				//target：div marked -> unmark
-				let itemName = document.createTextNode( target.children().text() )
-				target.html('').append(itemName);
-				target.removeAttr('mark')					
-				countItem()
-			}else{
-				//target：div unmark -> mark
-				let itemName = document.createTextNode( target.text() )
-				target.attr('mark', 'item-checked')
-				target.html(`<del></del>`).children().append(itemName)
-				countItem()
-			}
-		}else if( target.is('del') ){
-			//target：del（marded div's children）-> unmark
-			let itemName = document.createTextNode( target.text() )
-			target.parent().removeAttr('mark')
-			target.parent().html('').append(itemName)
-			countItem()
+		let index = target.parents('.row-item').attr('order')
+		if( list[index].mark === 0 ){
+			//未標記0 改為 已標記1
+			list[index].mark = 1
+		}else if( list[index].mark === 1){
+			//已標記1 改為 未標記0
+			list[index].mark = 0
 		}
-		if( $('#show').html() === 'Active' ){
-			showAtcive()
-		}
-	});
-
-/* delete item */
-	$('.listBlock').on('click', '.btn-delete', function(e){
-		let target = $(e.target).parent('.row-item')
-		target.fadeOut(400,function(){
-			target.remove('.row-item')
-			countItem()
-		})
+		render( pageNow() );
 	});
 
 });
+
+
+
+
+
+
+
+
+
+
